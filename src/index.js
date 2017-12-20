96442d2784192c9b7e215143193f9bf9ba59eaf0
DevNebulae <gamerkeeperson+github@gmail.com>
2017-12-20 21:41:31 +0100

    Update bootstrapping index with Redux Provider and App container

--- src/index.js
+++ src/index.js
@@ -1,2 +1,15 @@
 import "babel-polyfill"
 import "./styles.scss"
+
+import App from "./containers/App"
+import React from "react"
+import store from "./store"
+import { Provider } from "react-redux"
+import { render } from "react-dom"
+
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("react-root")
+)
