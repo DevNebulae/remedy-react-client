cbf815836f9aa186a90c339d8b9bf7ad5b766968
DevNebulae <gamerkeeperson+github@gmail.com>
2017-12-21 13:02:24 +0100

    Add TrackList component

---
+++ src/components/TrackList.jsx
@@ -0,0 +1,30 @@
+import { TrackType } from "../prop-types/track"
+import PropTypes from "prop-types"
+import React from "react"
+import Track from "./Track"
+
+const TrackList = tracks => {
+  if (tracks.loading) return <h1>Loading data</h1>
+  else if (tracks.error) return <h1>Error while loading data</h1>
+  else
+    return (
+      <ul>
+        {tracks.data.map((track, index) => (
+          <Track
+            key={`track-${track.id}`}
+            index={index}
+            track={track}
+            artists={track.artists}
+          />
+        ))}
+      </ul>
+    )
+}
+
+TrackList.propTypes = {
+  loading: PropTypes.bool,
+  error: PropTypes.bool,
+  data: PropTypes.arrayOf(TrackType)
+}
+
+export default TrackList
